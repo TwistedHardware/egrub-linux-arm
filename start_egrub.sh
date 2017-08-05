f852145cf0df0f71041909778cf485eb215b5bdb
Your Name <you@example.com>
2017-08-05 12:58:22 +0800

    Backup or restore egrub binary before starting egrub

--- start_egrub.sh
+++ start_egrub.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd /home/sos/egrubarm
+if [ -f "egrub" ]
+then
+  cp egrub egrub-bak
+else
+  if [ -f "egrub-bak" ]
+  then
+    cp egrub-bak egrub
+  fi
+fi
 exec /home/sos/egrubarm/egrub
